d5b3abbb98245ab5231a87b91735a497bc9e061f
Afrooz Bolboli <68796416+shigurehyung@users.noreply.github.com>
2025-11-04 22:20:38 +0100

    Hitingsets initials

--- Python/hittingsets.py
+++ Python/hittingsets.py
@@ -1,8 +1,33 @@
 def run_hitting_set_algorithm(conflict_sets):
+    # The smallest possible explanation for the observed faults
     """
     Algorithm that handles the entire process from conflict sets to hitting sets
 
     :param conflict_sets: list of conflict sets as list
     :return: the hitting sets and minimal hitting sets as list of lists
     """
+
+    # Check to see if the hitting set and conflicts have an intersection
+    def is_intersected(hitting_set, conflict):
+        return
+     
+    # Check to see if it is a minimal hitting set
+    def is_minimal(hitting_set, minimal_sets):
+        return 
+    
+    # Variables 
+    # Good representation for the tree structure
+    to_explore_nodes = [] #tree nodes
+    minimal_sets = []
+
+    # While there are still codes to explore go on
+    # Implementing hitting set tree algorithm
+    while():
+        break
+        # if is_intersected
+            #if is_minimal
+                #  add to minimal_sets
+
+        # Make Hitting sets which are possible combination of different conflicts 
+
     return None, None
